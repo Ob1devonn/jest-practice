384d7744e14832bf79adf3b7f9e422ed93aaa2ca
DeVonn <ob1devonn@gmail.com>
2023-10-31 18:47:53 -0400

    Create capitalize test and function

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function reverse(string) {
+  return;
+}
+module.exports = { sum, capitalize };
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,13 @@
-const sum = require("./index.js");
+const { sum, capitalize } = require("./index.js");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test("Capitalizes first letter of word", () => {
+  expect(capitalize("Test")).toBe("Test");
+});
+
+test("Returns reversed word", () => {
+  expect(reverse("word")).toBe("drow");
 });
