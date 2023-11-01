adb1e6f9cbde657d1828053bd53f5079fd3a10a1
DeVonn <ob1devonn@gmail.com>
2023-10-31 22:05:20 -0400

    Create static Calculator class and test it

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ function reverse(string) {
   return string.split("").reverse().join("");
 }
 
+class Calculator {
+  static add(a, b) {
+    return a + b;
+  }
+  static subtract(a, b) {
+    return a - b;
+  }
+  static divide(a, b) {
+    return a / b;
+  }
+  static multiply(a, b) {
+    return a * b;
+  }
+}
+
 console.log(reverse("drow"));
 
-module.exports = { sum, capitalize, reverse };
+module.exports = { sum, capitalize, reverse, Calculator };
--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-const { sum, capitalize, reverse } = require("./index.js");
+const { sum, capitalize, reverse, Calculator } = require("./index.js");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
@@ -10,4 +10,20 @@ test("Capitalizes first letter of word", () => {
 
 test("Returns reversed word", () => {
   expect(reverse("word")).toBe("drow");
+});
+
+test("Returns 1 + 2 to equal 3", () => {
+  expect(Calculator.add(1, 2)).toBe(3);
+});
+
+test("Returns 2 - 1 to equal 1", () => {
+  expect(Calculator.subtract(2, 1)).toBe(1);
+});
+
+test("Returns 4 / 2 to equal 2", () => {
+  expect(Calculator.divide(4, 2)).toBe(2);
+});
+
+test("Returns 2 * 2 to equal 4", () => {
+  expect(Calculator.multiply(2, 2)).toBe(4);
 });
